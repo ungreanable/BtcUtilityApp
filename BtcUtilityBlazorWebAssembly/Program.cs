7fb2a6c81232fb6880dae1d48f680284ffeb870b
UnGreanable <UnGreanable@DESKTOP-FLFNUPV>
2025-07-05 01:09:50 +0700

    Change Name to BtcUtilityBlazorWebAssembly

--- BtcUtilityBlazorWebAssembly/Program.cs
+++ BtcUtilityBlazorWebAssembly/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace BtcUtilityBlazorApp
+namespace BtcUtilityBlazorWebAssembly
 {
     public class Program
     {
